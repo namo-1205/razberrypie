6f45529f95e0dd27773a7889b398eb4290ea4ed9
namo-1205 <namo1205@korea.ac.kr>
2021-05-28 18:35:10 +0900

    code correction

--- main.py
+++ main.py
@@ -8,14 +8,62 @@ from gql.transport.aiohttp import AIOHTTPTransport
 import sqlite3
 import requests
 import json
+import serial
+from picamera import PiCamera
+from time import sleep
+
+class sensor:
+    def __init__(self, wei, csd, hum, tem):
+        self.wei = wei
+        self.csd = csd
+        self.hem = hum
+        self.tem = tem
+
+try:
+    ser = serial.Serial( port='COM4', baudrate=9600 )
+except:
+    ser.close()
+    ser = serial.Serial( port='COM4', baudrate=9600 )
+
+sensor_data = sensor(0, 0, 0, 0)
+
+while True:
+    if ser.readable():
+        res = ser.readline()
+        char = res.decode()[:len(res)-2] #개행문자제거
+
+        '''
+        serial.print( ,1) 모든 숫자는 소수점 1자리 가정. 
+        아두이노 측 출력 예시
+        Serial.print("wei: "); 출력문자는 공백 포함 5자
+        Serial.print(scale.get_units()*45, 1);
+        '''
+
+        if char[:5] == 'Wei: ':
+            wei = float(char[5:])
+            sensor_data.wei = wei
+            print('weight: ', wei )
+
+        elif char[:5] == 'CDS: ':
+            cds = float(char[5:])
+            sensor_data.cds = cds
+            #print('CDS_Sensor: ', cds)
+
+        elif char[:5] == "Hum: ":
+            hum = float(char[5:])
+            sensor_data.hum = hum
+            #print("Humidity: ", hum)
+
+        elif char[:5] == "Tem: ":
+            tem = float(char[5:])
+            sensor_data.tem = tem
+            #print("Temperature: ", tem)
+
+        else:
+            print("known", char[5:])
 
 
-def confirm(a, bb):
-    for i in bb:
-        for k in i['stocks']:
-            if k['name'] == a:
-                return True, k['tray_id']
-    return False, -1
+
 
 # Select your transport with a defined url endpoint
 transport = AIOHTTPTransport(url="http://ec2-3-36-171-69.ap-northeast-2.compute.amazonaws.com:8080/v1/graphql")
