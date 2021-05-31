aa108443c71d0fc75a484b6ef1bbd5b332a3353c
namo-1205 <namo1205@korea.ac.kr>
2021-05-31 10:38:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ camera = PiCamera()
 camera.start_preview()
 is_prev_light = True
 
-datetime1 = datetime.now()
+initial_time = datetime.now()
 
 while True:
     upload_result = upload_file()
@@ -17,19 +17,19 @@ while True:
     # Execute the query on the transport
     stock_name = upload_result['name'] # 재고 이름
 
-    (is_stock_exist, tray_id) = receive_tray_id(stock_name)
+    tray_id = receive_tray_id(stock_name)
 
     # Execute the query on the transport
 
     new_stock_info(tray_id, stock_name)
 
-    datetime2 = datetime.now()
+    now = datetime.now()
 
     (sensor_data, current_light) = info_from_sensor(camera, is_prev_light)
 
-    if datetime1.minute - datetime2.minute >= 5:
+    if now.minute - initial_time.minute >= 5:
         insert_sensor_info(stock_name, sensor_data)
-        datetime1 = datetime.now()
+        initial_time = datetime.now()
 
     result = fetch_tray_info()
     cache_stock_info(result)
