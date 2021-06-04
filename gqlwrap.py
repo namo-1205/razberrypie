6596e808b2e93548a8e24b3966aec704a010c0a4
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-04 14:24:00 +0900

    Split query and mutation

--- gqlwrap.py
+++ gqlwrap.py
@@ -4,20 +4,16 @@ from gql.transport.aiohttp import AIOHTTPTransport
 transport = AIOHTTPTransport(url="http://ec2-3-36-171-69.ap-northeast-2.compute.amazonaws.com:8080/v1/graphql")
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
-def receive_tray_id(stock_name):
-  query = gql('''query($stockName: String!){
-    stock(where: {name: {_eq: $stockName}}, order_by: {id: desc}, limit: 1){
-      created_at
-      tray_id
+def last_stock(tray_id):
+  query = gql('''query($trayId: Int!){
+    stock(where: {tray_id: {_eq: $trayId}}, limit: 1, order_by: {id: desc}){
       name
-      id
     }
   }''')
   result = client.execute(query, variable_values={
-    "stockName": stock_name
+    'trayId': tray_id
   })
-
-  return result['stock'][0]['tray_id'] if len(result['stock']) > 0 else None
+  return result['stock'][0] if len(result['stock']) > 0 else None
 
 def new_stock_info(tray_id, name):
   query = gql("""mutation ($object: stock_insert_input!) {
@@ -27,36 +23,13 @@ def new_stock_info(tray_id, name):
     }
   }
   """)
-  client.execute(query, variable_values = {
+  result = client.execute(query, variable_values = {
     "object": {
       "tray_id" : tray_id,
       "name": name
     }
   })
-
-def new_tray(name):
-  query = gql("""mutation ($object: tray_insert_input!) {
-      insert_tray_one(object: $object){
-      id
-      stocks{
-        id
-      }
-    }
-  }""")
-  client.execute(query, variable_values={
-    "object": {
-      "name": "your refridgerator",
-      "order_priority": 1,
-      "user_id": 1,
-      "stocks": {
-        "data": [
-          {
-              "name": name
-          }
-        ]
-      }
-    }
-  })
+  return result['insert_stock_one']['tray_id']
 
 def fetch_tray_info():
   query = gql('''query{
@@ -75,43 +48,29 @@ def fetch_tray_info():
   }''')
   return client.execute(query)
 
-def insert_sensor_info(stock_name, sensor_data):
-  query = gql('''query($stockName: String!){
-    stock(where: {name: {_eq: $stockName}}, order_by: {id: desc}, limit: 1){
+def insert_stock_with_sensor_info(stock_id, sensor_data):
+  mutation=gql("""mutation ($humdityTemperature: humidity_temperature_insert_input!, $weight: weight_insert_input) {
+    insert_humidity_temperature_one(object: $humdityTemperature){
+      stock_id
+      humidity
+      temperature
       created_at
-      tray_id
-      name
-      id
     }
-  }''')
-  result = client.execute(query, variable_values={
-    "stockName": stock_name
-  })
-  is_stock_exist = len(result['stock']) > 0
-  if is_stock_exist:
-    stock_id = result['stock'][0]['id']
-    mutation=gql("""mutation ($humdityTemperature: humidity_temperature_insert_input!, $weight: weight_insert_input) {
-      insert_humidity_temperature_one(object: $humdityTemperature){
-        stock_id
-        humidity
-        temperature
-        created_at
-      }
-      insert_weight_one(object: $weight){
-        stock_id
-        created_at
-        value
-      }
-    }""")
-    client.execute(mutation, variable_values={
-      "humdityTemperature": {
-          "stock_id": stock_id,
-          "humidity": sensor_data.humadity,
-          "temperature": sensor_data.temperature
-      },
-      "weight": {
+    insert_weight_one(object: $weight){
+      stock_id
+      created_at
+      value
+    }
+  }""")
+  client.execute(mutation, variable_values={
+    "humdityTemperature": {
         "stock_id": stock_id,
-        "value": sensor_data.weight
-      }
-    })
+        "humidity": sensor_data.humadity,
+        "temperature": sensor_data.temperature
+    },
+    "weight": {
+      "stock_id": stock_id,
+      "value": sensor_data.weight
+    }
+  })
   
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from gqlwrap import receive_tray_id, new_stock_info, insert_sensor_info, fetch_tray_info
+from gqlwrap import last_stock, new_stock_info, insert_stock_with_sensor_info, fetch_tray_info
 from sqlite_file import con, cache_stock_info, get_stocks_cached
 from upload_file import upload_file
 from picamera import PiCamera
@@ -10,33 +10,31 @@ camera.start_preview()
 is_prev_light = True
 
 initial_time = datetime.now()
+tray_id = 1
 
 while True:
-    upload_result = upload_file()
-
-    # Execute the query on the transport
-    stock_name = upload_result['name'] # 재고 이름
-
-    tray_id = receive_tray_id(stock_name)
-
-    # Execute the query on the transport
-
-    new_stock_info(tray_id, stock_name)
-
     now = datetime.now()
-
     (sensor_data, current_light) = info_from_sensor(camera, is_prev_light)
-
     if now.minute - initial_time.minute >= 5:
-        insert_sensor_info(stock_name, sensor_data)
+        upload_result = upload_file()
+
+        # Execute the query on the transport
+        stock_name = upload_result['name'] # 재고 이름
+
+        last_stock = last_stock(tray_id)
+        if last_stock['name'] != stock_name:
+            stock_id = new_stock_info(tray_id, stock_name)
+            insert_stock_with_sensor_info(tray_id, sensor_data)
+        else:
+            insert_sensor_info(stock_name, sensor_data)
         initial_time = datetime.now()
 
-    result = fetch_tray_info()
-    cache_stock_info(result)
+        result = fetch_tray_info()
+        cache_stock_info(result)
 
-    stocks = get_stocks_cached()
+        stocks = get_stocks_cached()
 
-    for stock in stocks:
-        print(stock)
+        for stock in stocks:
+            print(stock)
 
 con.close()
