5f66d3ffceb7e953a37d10f61e4fceb26f8736e2
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-09 14:10:47 +0900

    Notify user if stock is too old\n\nOnly if user notify firstly or before 1 day

--- sqlite_file.py
+++ sqlite_file.py
@@ -5,10 +5,13 @@ con = sqlite3.connect('./test.db')
 cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS stock1
                 (created_at, tray_id, name, id)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS notification_cached (created_at, route_kind, tray_id, stock_id)''')
 
 def cache_stock_info(server_query_result):
   records = [[(stock['created_at'], tray['id'], stock['name'], stock['id']) for stock in tray['stocks']] for tray in server_query_result['tray']]
   records = reduce(lambda till_now, current: till_now + current, records, [])
+  tray_id = set([stock[-3] for stock in records])
+  cur.execute('''DELETE FROM stock1 WHERE tray_id IN (seq)'''.format(seq=','['?']*len(tray_id)), tray_id)
   cur.executemany('''INSERT INTO stock1 (created_at, tray_id, name, id) VALUES (?, ?, ?, ?)''', records)
   con.commit()
 
@@ -19,5 +22,15 @@ def get_stocks_cached():
 
 def get_created_at_cached(now):
   cur.execute('''SELECT tray_id, id FROM stock1 WHERE JULIANDAY(%d) - JULIANDAY(created_at) >= 3''', now)
+  result = cur.fetchall()
+  return result
+
+def already_notified(time, stock_id, route_kind):
+  cur.execute('''SELECT created_at, route_kind, tray_id, stock_id FROM notification_cached WHERE JULIANDAY(?) - JULIANDAY(created_at) < 1 AND stock_id = ? AND route_kind = ?''', time, stock_id, route_kind)
+  result = cur.fetchall()
+  return len(result) > 0
+
+def get_too_old_stocks(now):
+  cur.execute('''SELECT created_at, tray_id, name, id FROM stock1 WHERE JULIANDAY(?) - JULIANDAY(created_at) >= 14''', now)
   result = cur.fetchall()
   return result
